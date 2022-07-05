e177d9e9cceec59461153c807d90c6f80a1fbf9a
ejay <ejay_young@163.com>
2022-07-05 18:39:04 +0800

    feat: auto insert route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,14 +3,15 @@ import Router from '@koa/router'
 import glob from 'glob'
 import path from 'path'
 
-let routers = []
-
-glob.sync(path.join(__dirname, '!(index.ts)')).forEach(file => {
-  console.log(file)
-})
-
 const router = new Router()
 
+Promise.all(
+  glob.sync(path.join(__dirname, '!(index.ts)')).map(file => import(file))
+).then(modules => {
+  modules.forEach(({ default: route }) => {
+    router.use(route.routes())
+  })
+})
 
 export {
   router
